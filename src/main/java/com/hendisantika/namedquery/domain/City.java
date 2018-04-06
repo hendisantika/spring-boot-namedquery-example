726791c83187cd36388b9bb6310dfce7dabb3274
Hendi Santika <hendisantika@gmail.com>
2018-04-06 07:05:06 +0700

    #4 - Add City Domain

---
+++ src/main/java/com/hendisantika/namedquery/domain/City.java
@@ -0,0 +1,102 @@
+package com.hendisantika.namedquery.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-namedquery-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/04/18
+ * Time: 07.02
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "cities")
+@NamedQuery(name = "City.findAllOrderedByNameDescending",
+        query = "SELECT c FROM City c ORDER BY c.name DESC")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private int population;
+
+    public City() {
+    }
+
+    public City(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        hash = 79 * hash + Objects.hashCode(this.name);
+        hash = 79 * hash + this.population;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final City other = (City) obj;
+        if (this.population != other.population) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("City{id=").append(id).append(", name=")
+                .append(name).append(", population=")
+                .append(population).append("}");
+
+        return builder.toString();
+    }
+}
+
