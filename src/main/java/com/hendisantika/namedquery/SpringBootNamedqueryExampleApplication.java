907cec17967d22b6695078daebe655b8295b498b
Hendi Santika <hendisantika@gmail.com>
2018-04-06 07:01:00 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/namedquery/SpringBootNamedqueryExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.namedquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNamedqueryExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNamedqueryExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hendisantika/namedquery/SpringBootNamedqueryExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.namedquery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootNamedqueryExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
